95d10cda79cdcb7547363f088358cfadba919386
Vincent Joy <vincentjoyhere@gmail.com>
2024-11-20 19:33:19 +0530

    Initial Commit

---
+++ SwiftfulSwiftUiInPractice/SwiftfulSwiftUiInPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulSwiftUiInPracticeApp.swift
+//  SwiftfulSwiftUiInPractice
+//
+//  Created by Vincent Joy on 20/11/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulSwiftUiInPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
