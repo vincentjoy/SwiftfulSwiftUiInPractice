e6ffb40a23c0d9cdf456be53459b2198cf1016f0
Vincent Joy <vincentjoyhere@gmail.com>
2024-11-23 22:49:10 +0530

    SongRowCell design complete

---
+++ SwiftfulSwiftUiInPractice/Spotify/Components/SongRowCell.swift
@@ -0,0 +1,57 @@
+import SwiftUI
+
+struct SongRowCell: View {
+    
+    var imageSize: CGFloat = 65
+    var imageName: String = Constants.randomImageURL
+    var title: String = "Song Title"
+    var subtitle: String? = "Song artist name"
+    
+    var onCellPressed: (() -> Void)?
+    var onEllipsisPressed: (() -> Void)?
+    
+    var body: some View {
+        HStack(spacing: 8) {
+            ImageLoaderView(urlString: imageName)
+                .frame(width: imageSize, height: imageSize)
+            
+            VStack(alignment: .leading, spacing: 4) {
+                Text(title)
+                    .font(.body)
+                    .fontWeight(.medium)
+                    .foregroundStyle(.spotifyWhite)
+                if let subtitle {
+                    Text(subtitle)
+                        .font(.callout)
+                        .foregroundStyle(.spotifyLightGray)
+                }
+            }
+            .lineLimit(2)
+            .frame(maxWidth: .infinity, alignment: .leading)
+            
+            Image(systemName: "ellipsis")
+                .font(.subheadline)
+                .foregroundStyle(.spotifyWhite)
+                .padding(16)
+                .background(Color.black.opacity(0.001))
+                .onTapGesture {
+                    onEllipsisPressed?()
+                }
+                
+        }
+        .foregroundStyle(.red)
+    }
+}
+
+#Preview {
+    ZStack {
+        Color.black.ignoresSafeArea()
+        VStack {
+            SongRowCell()
+            SongRowCell()
+            SongRowCell()
+            SongRowCell()
+            SongRowCell()
+        }
+    }
+}
