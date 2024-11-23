93ce3a63a41b63b201adae289fe8fbd2c9bd0d79
Vincent Joy <vincentjoyhere@gmail.com>
2024-11-23 23:01:21 +0530

    cells added to the SpotifyPlayListView

--- SwiftfulSwiftUiInPractice/Spotify/Core/SpotifyPlayListView.swift
+++ SwiftfulSwiftUiInPractice/Spotify/Core/SpotifyPlayListView.swift
@@ -5,6 +5,8 @@ struct SpotifyPlayListView: View {
     var product: Product = .mock
     var user: User = .mock
     
+    @State private var products: [Product] = []
+    
     var body: some View {
         ZStack {
             Color.spotifyBlack.ignoresSafeArea()
@@ -28,9 +30,34 @@ struct SpotifyPlayListView: View {
                         onPlayPressed: nil
                     )
                     .padding(.horizontal, 16)
+                    
+                    ForEach(products) { product in
+                        SongRowCell(
+                            imageSize: 60,
+                            imageName: product.firstImage,
+                            title: product.title,
+                            subtitle: product.brand) {
+                                
+                            } onEllipsisPressed: {
+                                
+                            }
+                            .padding(.leading, 16)
+                    }
                 }
             }
             .scrollIndicators(.hidden)
+        }
+        .task {
+            await getData()
+        }
+        .toolbar(.hidden, for: .navigationBar)
+    }
+    
+    private func getData() async {
+        do {
+            products = try await DataBaseHelper().getProducts()
+        } catch {
+            
         }
     }
 }
