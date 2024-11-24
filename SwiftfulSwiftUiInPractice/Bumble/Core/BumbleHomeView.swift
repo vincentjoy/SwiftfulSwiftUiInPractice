66e33587eeefd2c8bf5a3e0d0f701297f85815d9
Vincent Joy <vincentjoyhere@gmail.com>
2024-11-24 19:54:24 +0530

    header design complete

---
+++ SwiftfulSwiftUiInPractice/Bumble/Core/BumbleHomeView.swift
@@ -0,0 +1,52 @@
+import SwiftUI
+
+struct BumbleHomeView: View {
+    var body: some View {
+        ZStack {
+            Color.bumbleWhite.ignoresSafeArea()
+            VStack(spacing: 8) {
+                header
+            }
+        }
+    }
+    
+    private var header: some View {
+        HStack(spacing: 8) {
+            HStack(spacing: 0) {
+                Image(systemName: "line.horizontal.3")
+                    .padding(8)
+                    .background(Color.black.opacity(0.001))
+                    .onTapGesture {
+                        
+                    }
+                Image(systemName: "arrow.uturn.left")
+                    .padding(8)
+                    .background(Color.black.opacity(0.001))
+                    .onTapGesture {
+                        
+                    }
+            }
+            .frame(maxWidth: .infinity, alignment: .leading)
+            
+            Text("Bumble")
+                .font(.title)
+                .foregroundColor(.bumbleYellow)
+                .frame(maxWidth: .infinity, alignment: .center)
+            
+            Image(systemName: "slider.horizontal.3")
+                .padding(8)
+                .background(Color.black.opacity(0.001))
+                .frame(maxWidth: .infinity, alignment: .trailing)
+                .onTapGesture {
+                    
+                }
+        }
+        .font(.title2)
+        .fontWeight(.medium)
+        .foregroundStyle(.bumbleBlack)
+    }
+}
+
+#Preview {
+    BumbleHomeView()
+}
