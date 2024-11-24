6983dabd61eed40e7e823b083b6c06561ab9e6ce
Vincent Joy <vincentjoyhere@gmail.com>
2024-11-24 19:03:28 +0530

    feature - swipe left to go back to previous screen - added

--- SwiftfulSwiftUiInPractice/SwiftfulSwiftUiInPracticeApp.swift
+++ SwiftfulSwiftUiInPractice/SwiftfulSwiftUiInPracticeApp.swift
@@ -18,3 +18,14 @@ struct SwiftfulSwiftUiInPracticeApp: App {
         }
     }
 }
+
+extension UINavigationController: @retroactive UIGestureRecognizerDelegate {
+    override open func viewDidLoad() {
+        super.viewDidLoad()
+        interactivePopGestureRecognizer?.delegate = self
+    }
+
+    public func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
+        return viewControllers.count > 1
+    }
+}
