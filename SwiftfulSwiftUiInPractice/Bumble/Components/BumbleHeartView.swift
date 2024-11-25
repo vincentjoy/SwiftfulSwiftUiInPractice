a77cb93cad42f230df79923786d334d7cbc5a6cd
Vincent Joy <vincentjoyhere@gmail.com>
2024-11-25 14:52:21 +0530

    BumbleHeartView design complete

--- SwiftfulSwiftUiInPractice/Bumble/Components/BumbleHeartView.swift
+++ SwiftfulSwiftUiInPractice/Bumble/Components/BumbleHeartView.swift
@@ -1,15 +1,19 @@
-//
-//  BumbleHeartView.swift
-//  SwiftfulSwiftUiInPractice
-//
-//  Created by Vincent Joy on 25/11/24.
-//
-
 import SwiftUI
 
 struct BumbleHeartView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Circle()
+                .fill(.bumbleYellow)
+                .frame(width: 40, height: 40)
+            Image(systemName: "bubble.fill")
+                .foregroundStyle(.bumbleBlack)
+                .font(.system(size: 22))
+                .offset(y: 2)
+            Image(systemName: "heart.fill")
+                .foregroundStyle(.bumbleYellow)
+                .font(.system(size: 10))
+        }
     }
 }
 
