3723052ac45bd8e93e9211bad4c4d2f2b309ba28
Vincent Joy <vincentjoyhere@gmail.com>
2024-11-25 20:51:35 +0530

    InterestPillView design complete

---
+++ SwiftfulSwiftUiInPractice/Bumble/Components/InterestPillView.swift
@@ -0,0 +1,34 @@
+import SwiftUI
+
+struct InterestPillView: View {
+    
+    var iconName: String? = "heart.fill"
+    var emoji: String? = "🍄"
+    var text: String = "Graduate Degree"
+    
+    var body: some View {
+        HStack(spacing: 4) {
+            if let iconName {
+                Image(systemName: iconName)
+            }
+            if let emoji {
+                Text(emoji)
+            }
+            Text(text)
+        }
+        .font(.callout)
+        .fontWeight(.medium)
+        .padding(.vertical, 6)
+        .padding(.horizontal, 12)
+        .foregroundStyle(.bumbleBlack)
+        .backgroundStyle(.bumbleLightYellow)
+        .cornerRadius(32)
+    }
+}
+
+#Preview {
+    VStack {
+        InterestPillView(iconName: nil)
+        InterestPillView()
+    }
+}
