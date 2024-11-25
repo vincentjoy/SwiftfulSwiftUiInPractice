c35b5fb8c2f10759c724d8ddeb5430b3baed693e
Vincent Joy <vincentjoyhere@gmail.com>
2024-11-25 15:04:46 +0530

    bumble card view's top section is complete with adjusting height to the screen

--- SwiftfulSwiftUiInPractice/Bumble/Components/BumbleCardView.swift
+++ SwiftfulSwiftUiInPractice/Bumble/Components/BumbleCardView.swift
@@ -1,36 +1,62 @@
 import SwiftUI
+import SwiftfulUI
 
 struct BumbleCardView: View {
     
     var user: User = .mock
+    @State private var cardFrame: CGRect = .zero
     
     var body: some View {
         ScrollView(.vertical) {
             LazyVStack(spacing: 0) {
-                ZStack(alignment: .bottomLeading) {
-                    ImageLoaderView(urlString: user.image)
-                    VStack(alignment: .leading, spacing: 8) {
-                        Text("\(user.firstName), \(user.age)")
-                            .font(.largeTitle)
-                            .fontWeight(.semibold)
-                        HStack(spacing: 4) {
-                            Image(systemName: "suitcase")
-                            Text("Works at \(user.work)")
-                        }
-                        HStack(spacing: 4) {
-                            Image(systemName: "graduationcap")
-                            Text(user.work)
-                        }
-                    }
-                    .padding(24)
-                    .font(.callout)
-                    .fontWeight(.medium)
-                    .foregroundStyle(.bumbleWhite)
-                }
-                .frame(height: 600)
+                headerCell
+                    .frame(height: cardFrame.height)
             }
         }
         .scrollIndicators(.hidden)
+        .cornerRadius(32)
+        .readingFrame { frame in
+            cardFrame = frame
+        }
+    }
+    
+    var headerCell: some View {
+        ZStack(alignment: .bottomLeading) {
+            ImageLoaderView(urlString: user.image)
+            VStack(alignment: .leading, spacing: 8) {
+                Text("\(user.firstName), \(user.age)")
+                    .font(.largeTitle)
+                    .fontWeight(.semibold)
+                HStack(spacing: 4) {
+                    Image(systemName: "suitcase")
+                    Text("Works at \(user.work)")
+                }
+                HStack(spacing: 4) {
+                    Image(systemName: "graduationcap")
+                    Text(user.work)
+                }
+                BumbleCardView()
+                    .onTapGesture {
+                        print("111")
+                    }
+            }
+            .padding(24)
+            .font(.callout)
+            .fontWeight(.medium)
+            .foregroundStyle(.bumbleWhite)
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .background(
+                LinearGradient(
+                    colors: [
+                        .bumbleBlack.opacity(0),
+                        .bumbleBlack.opacity(0.6),
+                        .bumbleBlack.opacity(0.6)
+                    ],
+                    startPoint: .top,
+                    endPoint: .bottom
+                )
+            )
+        }
     }
 }
 
