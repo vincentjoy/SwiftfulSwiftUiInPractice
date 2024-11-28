d76e920c56d583eb2db104b8e8d42ec1c37dc77b
Vincent Joy <vincentjoyhere@gmail.com>
2024-11-28 21:24:48 +0530

    NetflixMovieCell design complete

--- SwiftfulSwiftUiInPractice/Netflix/Components/NetflixMovieCell.swift
+++ SwiftfulSwiftUiInPractice/Netflix/Components/NetflixMovieCell.swift
@@ -1,10 +1,3 @@
-//
-//  NetflixMovieCell.swift
-//  SwiftfulSwiftUiInPractice
-//
-//  Created by Vincent Joy on 28/11/24.
-//
-
 import SwiftUI
 
 struct NetflixMovieCell: View {
@@ -17,54 +10,70 @@ struct NetflixMovieCell: View {
     var topTenRanking: Int?
     
     var body: some View {
-        ZStack(alignment: .bottom) {
-            ImageLoaderView(urlString: imageName)
-            VStack(spacing: 0) {
-                if let title,
-                    let firstWord = title.components(separatedBy: " ").first {
-                    Text(firstWord)
-                        .font(.subheadline)
-                        .fontWeight(.medium)
-                        .lineLimit(1)
-                }
-                Text("Recently Added")
-                    .padding(.horizontal, 4)
-                    .padding(.vertical, 2)
-                    .padding(.bottom, 2)
-                    .frame(maxWidth: .infinity)
-                    .background(.netflixRed)
-                    .cornerRadius(2)
-                    .offset(y: 2)
-                    .lineLimit(1)
-                    .font(.caption2)
-                    .fontWeight(.bold)
-                    .minimumScaleFactor(0.1)
-                    .padding(.horizontal, 8)
-                    .opacity(isRecentlyAdded ? 1 : 0)
+        HStack(alignment: .bottom, spacing: -8) {
+            if let topTenRanking {
+                Text("\(topTenRanking)")
+                    .font(.system(size: 100, weight: .medium, design: .serif))
+                    .offset(y: 20)
             }
-            .padding(.top, 6)
-            .background(
-                LinearGradient(
-                    colors: [
-                        .netflixBlack.opacity(0),
-                        .netflixBlack.opacity(0.3),
-                        .netflixBlack.opacity(0.4),
-                        .netflixBlack.opacity(0.4)
-                    ],
-                    startPoint: .top,
-                    endPoint: .bottom
+    
+            ZStack(alignment: .bottom) {
+                ImageLoaderView(urlString: imageName)
+                VStack(spacing: 0) {
+                    if let title,
+                        let firstWord = title.components(separatedBy: " ").first {
+                        Text(firstWord)
+                            .font(.subheadline)
+                            .fontWeight(.medium)
+                            .lineLimit(1)
+                    }
+                    Text("Recently Added")
+                        .padding(.horizontal, 4)
+                        .padding(.vertical, 2)
+                        .padding(.bottom, 2)
+                        .frame(maxWidth: .infinity)
+                        .background(.netflixRed)
+                        .cornerRadius(2)
+                        .offset(y: 2)
+                        .lineLimit(1)
+                        .font(.caption2)
+                        .fontWeight(.bold)
+                        .minimumScaleFactor(0.1)
+                        .padding(.horizontal, 8)
+                        .opacity(isRecentlyAdded ? 1 : 0)
+                }
+                .padding(.top, 6)
+                .background(
+                    LinearGradient(
+                        colors: [
+                            .netflixBlack.opacity(0),
+                            .netflixBlack.opacity(0.3),
+                            .netflixBlack.opacity(0.4),
+                            .netflixBlack.opacity(0.4)
+                        ],
+                        startPoint: .top,
+                        endPoint: .bottom
+                    )
                 )
-            )
+            }
+            .cornerRadius(4)
+            .frame(width: width, height: height)
         }
-        .cornerRadius(4)
-        .frame(width: width, height: height)
         .foregroundStyle(.netflixWhite)
     }
 }
 
 #Preview {
-    VStack {
-        NetflixMovieCell(isRecentlyAdded: false)
-        NetflixMovieCell()
+    ZStack {
+        Color.black.ignoresSafeArea()
+        ScrollView {
+            VStack {
+                NetflixMovieCell(isRecentlyAdded: false)
+                NetflixMovieCell()
+                NetflixMovieCell(topTenRanking: 1)
+                NetflixMovieCell(topTenRanking: 6)
+                NetflixMovieCell(topTenRanking: 10)
+            }
+        }
     }
 }
