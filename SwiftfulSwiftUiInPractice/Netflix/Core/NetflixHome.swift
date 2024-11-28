f54a509b6f6db4f7a82af99d0f26d928d3c2f472
Vincent Joy <vincentjoyhere@gmail.com>
2024-11-28 14:14:02 +0530

    colour codes added for the new netflix project, along with its folder structure

---
+++ SwiftfulSwiftUiInPractice/Netflix/Core/NetflixHome.swift
@@ -0,0 +1,18 @@
+//
+//  NetflixHome.swift
+//  SwiftfulSwiftUiInPractice
+//
+//  Created by Vincent Joy on 28/11/24.
+//
+
+import SwiftUI
+
+struct NetflixHome: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NetflixHome()
+}
