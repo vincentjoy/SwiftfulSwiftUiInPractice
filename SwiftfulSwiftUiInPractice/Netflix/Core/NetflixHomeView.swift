5af5e1e9a70eab67fbb1a7f75de2fa98a5bbca06
Vincent Joy <vincentjoyhere@gmail.com>
2024-11-28 17:51:46 +0530

    renamed to NetflixHomeView

--- SwiftfulSwiftUiInPractice/Netflix/Core/NetflixHomeView.swift
+++ SwiftfulSwiftUiInPractice/Netflix/Core/NetflixHomeView.swift
@@ -1,15 +1,47 @@
-//
-//  NetflixHomeView.swift
-//  SwiftfulSwiftUiInPractice
-//
-//  Created by Vincent Joy on 28/11/24.
-//
-
 import SwiftUI
 
 struct NetflixHomeView: View {
+    
+    @State private var filters = FilterModel.mockArray
+    @State private var selectedFilter: FilterModel?
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Color.netflixBlack.ignoresSafeArea()
+            VStack(spacing: 0) {
+                headerView
+                    .padding(.horizontal, 16)
+                NetflixFilterBarView(
+                    filters: filters,
+                    selectedFilter: selectedFilter) { newFilter in
+                        selectedFilter = newFilter
+                    } onXMarkPressed: {
+                        selectedFilter = nil
+                    }
+                    .padding(.top, 16)
+                Spacer()
+            }
+        }
+        .foregroundStyle(.netflixWhite)
+    }
+    
+    private var headerView: some View {
+        HStack(spacing: 0) {
+            Text("For Tao")
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .font(.title)
+            HStack(spacing: 16) {
+                Image(systemName: "tv.badge.wifi")
+                    .onTapGesture {
+                        
+                    }
+                Image(systemName: "magnifyingglass")
+                    .onTapGesture {
+                        
+                    }
+            }
+            .font(.title2)
+        }
     }
 }
 
