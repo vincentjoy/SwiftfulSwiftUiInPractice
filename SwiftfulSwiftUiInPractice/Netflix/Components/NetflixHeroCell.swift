222c8a353b367c6c84d9e0e8cd6cd7f2ef09bc8c
Vincent Joy <vincentjoyhere@gmail.com>
2024-11-28 17:49:08 +0530

    NetflixHeroCell design complete

--- SwiftfulSwiftUiInPractice/Netflix/Components/NetflixHeroCell.swift
+++ SwiftfulSwiftUiInPractice/Netflix/Components/NetflixHeroCell.swift
@@ -1,11 +1,5 @@
-//
-//  NetflixHeroCell.swift
-//  SwiftfulSwiftUiInPractice
-//
-//  Created by Vincent Joy on 28/11/24.
-//
-
 import SwiftUI
+import SwiftfulUI
 
 struct NetflixHeroCell: View {
     
@@ -18,8 +12,88 @@ struct NetflixHeroCell: View {
     var onMyListTap: (() -> Void)?
     
     var body: some View {
-        Rectangle()
-            .aspectRatio(0.8, contentMode: .fit)
+        ZStack(alignment: .bottom) {
+            ImageLoaderView(urlString: imageName)
+            VStack(spacing: 16) {
+                VStack(spacing: 0) {
+                    if isNetflixFilm {
+                        HStack(spacing: 8) {
+                            Text("N")
+                                .foregroundStyle(.netflixRed)
+                                .font(.largeTitle)
+                                .fontWeight(.black)
+                            Text("FILM")
+                                .kerning(3)
+                                .font(.subheadline)
+                                .fontWeight(.semibold)
+                                .foregroundStyle(.netflixWhite)
+                        }
+                    }
+                    Text(title)
+                        .font(.system(size: 50, weight: .medium, design: .serif))
+                }
+                
+                HStack(spacing: 8) {
+                    ForEach(categories, id: \.self) { category in
+                        Text(category)
+                            .font(.callout)
+                        if category != categories.last {
+                            Circle()
+                                .frame(width: 4, height: 4)
+                        }
+                    }
+                }
+                
+                HStack(spacing: 16) {
+                    HStack {
+                        Image(systemName: "play.fill")
+                        Text("Play")
+                    }
+                    .frame(maxWidth: .infinity)
+                    .padding(.vertical, 8)
+                    .foregroundStyle(.netflixDarkGray)
+                    .background(.netflixWhite)
+                    .cornerRadius(4)
+                    .asButton(.press) {
+                        onPlayTap?()
+                    }
+                    
+                    HStack {
+                        Image(systemName: "plus")
+                        Text("My List")
+                    }
+                    .frame(maxWidth: .infinity)
+                    .padding(.vertical, 8)
+                    .foregroundStyle(.netflixWhite)
+                    .background(.netflixDarkGray)
+                    .cornerRadius(4)
+                    .asButton(.press) {
+                        onMyListTap?()
+                    }
+                }
+                .font(.callout)
+                .fontWeight(.medium)
+            }
+            .padding(24)
+            .background(
+                LinearGradient(
+                    colors: [
+                        .netflixBlack.opacity(0),
+                        .netflixBlack.opacity(0.4),
+                        .netflixBlack.opacity(0.4),
+                        .netflixBlack.opacity(0.4)
+                    ],
+                    startPoint: .top,
+                    endPoint: .bottom
+                )
+            )
+        }
+        .foregroundStyle(.netflixWhite)
+        .cornerRadius(10)
+        .aspectRatio(0.8, contentMode: .fit)
+        .asButton {
+            onBackgroundTap?()
+        }
     }
 }
 
