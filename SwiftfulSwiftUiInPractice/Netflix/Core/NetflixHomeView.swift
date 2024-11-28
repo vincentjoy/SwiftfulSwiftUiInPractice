6e177366157fbc33e94a66abb5f764951dd1eb9f
Vincent Joy <vincentjoyhere@gmail.com>
2024-11-28 17:51:20 +0530

    renmaed to NetflixHomeView

---
+++ SwiftfulSwiftUiInPractice/Netflix/Core/NetflixHomeView.swift
@@ -0,0 +1,18 @@
+//
+//  NetflixHomeView.swift
+//  SwiftfulSwiftUiInPractice
+//
+//  Created by Vincent Joy on 28/11/24.
+//
+
+import SwiftUI
+
+struct NetflixHomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NetflixHomeView()
+}
