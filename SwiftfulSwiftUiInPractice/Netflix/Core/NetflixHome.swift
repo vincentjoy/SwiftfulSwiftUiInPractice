718fbeb137a4181bf8729dcb43128e4c9ee017b1
Vincent Joy <vincentjoyhere@gmail.com>
2024-11-28 14:26:07 +0530

    header added

--- SwiftfulSwiftUiInPractice/Netflix/Core/NetflixHome.swift
+++ SwiftfulSwiftUiInPractice/Netflix/Core/NetflixHome.swift
@@ -1,15 +1,35 @@
-//
-//  NetflixHome.swift
-//  SwiftfulSwiftUiInPractice
-//
-//  Created by Vincent Joy on 28/11/24.
-//
-
 import SwiftUI
 
 struct NetflixHome: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Color.netflixBlack.ignoresSafeArea()
+            VStack(spacing: 0) {
+                headerView
+                    .padding(.horizontal, 16)
+                Spacer()
+            }
+        }
+        .foregroundStyle(.netflixWhite)
+    }
+    
+    private var headerView: some View {
+        HStack(spacing: 0) {
+            Text("For Tao")
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .font(.title)
+            HStack(spacing: 16) {
+                Image(systemName: "tv.badge.wifi")
+                    .onTapGesture {
+                        
+                    }
+                Image(systemName: "magnifyingglass")
+                    .onTapGesture {
+                        
+                    }
+            }
+            .font(.title2)
+        }
     }
 }
 
