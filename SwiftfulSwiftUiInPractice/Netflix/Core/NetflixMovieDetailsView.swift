c2a7940c5d12346d0f9e4b7fe3ee46e609bb9694
Vincent Joy <vincentjoyhere@gmail.com>
2024-11-29 21:24:37 +0530

    NetflixMovieDetailsView initial setup done

---
+++ SwiftfulSwiftUiInPractice/Netflix/Core/NetflixMovieDetailsView.swift
@@ -0,0 +1,28 @@
+//
+//  NetflixMovieDetailsView.swift
+//  SwiftfulSwiftUiInPractice
+//
+//  Created by Vincent Joy on 29/11/24.
+//
+
+import SwiftUI
+
+struct NetflixMovieDetailsView: View {
+    
+    var product: Product = .mock
+    
+    var body: some View {
+        ZStack {
+            Color.netflixBlack.ignoresSafeArea()
+            Color.netflixDarkGray.opacity(0.3).ignoresSafeArea()
+            
+            VStack(spacing: 0) {
+                <#code#>
+            }
+        }
+    }
+}
+
+#Preview {
+    NetflixMovieDetailsView()
+}
