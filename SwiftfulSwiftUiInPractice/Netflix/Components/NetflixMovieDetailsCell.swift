548eae568b687484a87da8255a0fbc70cf12297e
Vincent Joy <vincentjoyhere@gmail.com>
2024-11-29 22:13:47 +0530

    NetflixMovieDetailsCell mock data needed for the design added

--- SwiftfulSwiftUiInPractice/Netflix/Components/NetflixMovieDetailsCell.swift
+++ SwiftfulSwiftUiInPractice/Netflix/Components/NetflixMovieDetailsCell.swift
@@ -1,18 +1,29 @@
-//
-//  NetflixMovieDetailsCell.swift
-//  SwiftfulSwiftUiInPractice
-//
-//  Created by Vincent Joy on 29/11/24.
-//
-
 import SwiftUI
 
 struct NetflixMovieDetailsCell: View {
+    
+    var title: String = "Breaking Bad"
+    var isNew: Bool = true
+    var yearReleased: String = "2008"
+    var seasonCount: Int? = 2
+    var hasClosedCaption: Bool = true
+    var isTopTen: Int? = 4
+    var descriptionText: String = "Walter White, a chemistry teacher, discovers that he has cancer and decides to get into the meth-making business to repay his medical debts. His priorities begin to change when he partners with Jesse."
+    var casr: String = "Cast: Bryan Cranston, Aaron Paul, Anna Gunn"
+    var onPlayTap: (() -> Void)?
+    var onDownloadTap: (() -> Void)?
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            
+        }
+        .foregroundStyle(.netflixWhite)
     }
 }
 
 #Preview {
-    NetflixMovieDetailsCell()
+    ZStack {
+        Color.black.ignoresSafeArea()
+        NetflixMovieDetailsCell()
+    }
 }
