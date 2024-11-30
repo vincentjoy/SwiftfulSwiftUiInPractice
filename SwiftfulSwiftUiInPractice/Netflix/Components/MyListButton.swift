9f56e73657901ee2ec86be270e743ada67d865d6
Vincent Joy <vincentjoyhere@gmail.com>
2024-11-30 09:34:18 +0530

    MyListButton added

---
+++ SwiftfulSwiftUiInPractice/Netflix/Components/MyListButton.swift
@@ -0,0 +1,53 @@
+//
+//  MyListButton.swift
+//  SwiftfulSwiftUiInPractice
+//
+//  Created by Vincent Joy on 30/11/24.
+//
+
+import SwiftUI
+
+struct MyListButton: View {
+    
+    var isMyList: Bool = true
+    var onButtonTap: (()->Void)?
+    
+    var body: some View {
+        VStack {
+            ZStack {
+                Image(systemName: "checkmark")
+                    .opacity(isMyList ? 1 : 0)
+                    .rotationEffect(Angle(degrees: isMyList ? 0 : 180))
+                Image(systemName: "plus")
+                    .opacity(isMyList ? 0 : 1)
+                    .rotationEffect(Angle(degrees: isMyList ? 180 : 0))
+            }
+            .font(.title)
+            Text("My List")
+                .font(.caption)
+                .foregroundStyle(.netflixLightGray)
+        }
+        .foregroundStyle(.netflixWhite)
+        .padding(8)
+        .background(Color.black.opacity(0.001))
+        .animation(.bouncy, value: isMyList)
+        .onTapGesture {
+            onButtonTap?()
+        }
+    }
+}
+
+fileprivate struct MyListButtonPreview: View {
+    
+    @State private var isMyList: Bool = true
+    
+    var body: some View {
+        MyListButton(isMyList: isMyList) {
+            isMyList.toggle()
+        }
+    }
+}
+
+#Preview {
+    MyListButtonPreview()
+}
